6af16114a68caa313d810f97928a4a6127e0cadc
alaahahi <alaa.hahe@gmail.com>
2026-07-30 11:59:44 +0300

    ش

--- public/build/assets/useLocale-C-6Sm6s0.js
+++
@@ -1,1 +0,0 @@
-import{aA as l,ah as c}from"./app-DzaA_SvC.js";function i(){const e=l(),{locale:o,availableLocales:a,direction:s,isRtl:t}=c(e);return{locale:o,availableLocales:a,direction:s,isRtl:t,setLocale:e.setLocale.bind(e)}}export{i as u};
